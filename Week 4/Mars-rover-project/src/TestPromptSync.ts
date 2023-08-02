116f81183c9042c26ee5dea398366c8fe591504a
Camh-git <cameronhillier1@gmail.com>
2023-08-02 20:47:04 +0100

    Pre-promptsync save
    Switiching over to promptscript to improve the code, saving current state incase I need to roll back

---
+++ Week 4/Mars-rover-project/src/TestPromptSync.ts
@@ -0,0 +1,4 @@
+const ps = require("prompt-sync");
+
+const a = ps("please enter your name:");
+console.log(`Hello: ${a}`);
--- Week 4/Mars-rover-project/src/index.ts
+++ Week 4/Mars-rover-project/src/index.ts
@@ -1,14 +1,15 @@
 import { Grid, CoOrdinate } from "./Types/Map";
-import { Vehicle } from "./Types/Vehicles";
+import * as Vics from "./Types/Vehicles";
 import { ToolKit, Tool } from "./Types/Tool";
 const readline = require("readline");
 
 //This file is used to handle the user input commands and send it off to the appropriate vehicle
 export let signalDelay: number = 0;
 type roverInstruction = "L" | "R" | "M";
-let depolyedVics: Array<Vehicle> = [];
-let selectedVic: Vehicle;
+let depolyedVics: Array<Vics.Vehicle> = [];
+let selectedVic: Vics.Vehicle;
 let map: Grid = { XMax: 0, YMax: 0, blockedTiles: [] };
+let exit = false;
 
 /* Starting up and handling user input */
 const rl = readline.createInterface({
@@ -58,13 +59,53 @@ export function startUp(input: string) {
     `Default rover: ${depolyedVics[0].name} deployed at: ${depolyedVics[0].position}`
   );
   switchVics("Rover1", depolyedVics);
+  awaitInput();
+}
+
+/*Input handling functions*/
+
+export function awaitInput() {
+  while (!exit) {
+    console.log(`Please enter a command, current vehicle: ${selectedVic.name}`);
+    rl.question("", (answer: string) => {
+      //commands without parameters
+      switch (answer.toUpperCase()) {
+        case "EXIT":
+          exit = true;
+          break;
+        case "L":
+          Vics.rotate("L", selectedVic);
+          break;
+        case "R":
+          Vics.rotate("R", selectedVic);
+          break;
+        case "M":
+          switch (selectedVic.vicType) {
+            case "Rover":
+              Vics.moveRover(selectedVic, map);
+              break;
+            case "Helicopter":
+              break;
+            case "Plane":
+              break;
+          }
+          break;
+        case "LISTVICS":
+        case "LIST":
+        case "VEHICLES":
+        case "VICLIST":
+          console.log(listVics(depolyedVics));
+          break;
+      }
+    });
+  }
 }
 
 export function move() {
   //Takes the movement command, processes it and sends it to the right vic
 }
 
-export function switchVics(vicName: string, vicList: Array<Vehicle>) {
+export function switchVics(vicName: string, vicList: Array<Vics.Vehicle>) {
   //swaps the active vic with the input vic
   vicList.forEach((vic) => {
     if (vic.name === "vicName") {
@@ -74,14 +115,18 @@ export function switchVics(vicName: string, vicList: Array<Vehicle>) {
   });
 }
 
-export function listVics(vicList: Array<Vehicle>): string {
+export function listVics(vicList: Array<Vics.Vehicle>): string {
+  //lists all vehicles in the given array, returns a string so it can be tested
   let list: string = "";
   vicList.forEach((vic) => {
     list += `${vicList.indexOf(vic)}:${vic.name}, ${vic.vicType}\n`;
   });
   return list;
 }
-export function addVic() {}
+export function addVic() {
+  //gets user input for each property and pushes the new object to depolyedVics
+  depolyedVics.push();
+}
 
 export function listTools() {
   //Lists the tools available to the selected vehicle
@@ -96,5 +141,5 @@ export function delay(length: number) {
     signalDelay = length;
   }
 }
-export function vicHelp(type: Vehicle) {}
+export function vicHelp(type: Vics.Vehicle) {}
 export function help() {}
