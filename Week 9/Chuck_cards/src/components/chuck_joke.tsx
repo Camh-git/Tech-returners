788eb57d37d25c3220311b1d81293816bc891d6f
Camh-git <cameronhillier1@gmail.com>
2023-10-02 21:51:52 +0100

    fix: chuck card folder
    I forgot to put the chuck cards in a folder, now fixed

---
+++ Week 9/Chuck_cards/src/components/chuck_joke.tsx
@@ -0,0 +1,8 @@
+interface ChuckJokeProps {
+  id: number;
+  joke: string;
+}
+
+const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>;
+
+export default ChuckJoke;
--- Week 9/src/components/chuck_joke.tsx
+++
@@ -1,8 +0,0 @@
-interface ChuckJokeProps {
-  id: number;
-  joke: string;
-}
-
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>;
-
-export default ChuckJoke;
